f11ccf967315ad8e3ed83b0b953063812d73f646
George Typaldos <george.typaldos@yale.edu>
2024-12-02 15:46:50 +0000

    examples modification

---
+++ examples/dot_out.rs
@@ -0,0 +1,9 @@
+use jraph::graph_gen::from_json;
+
+fn main() {
+    let graph_file = "examples/graphs/graph.json";
+
+    let graph = from_json(graph_file).unwrap();
+    let dot_out = graph.generate_dot();
+    println!("{}", dot_out);
+}
