b26273048af8bacb570e32ddfaaa6917cb0ff262
George Typaldos <george.typaldos@yale.edu>
2025-01-14 20:28:03 +0000

    import fix

--- jraph-py/examples/python_ex1.py
+++ jraph-py/examples/python_ex1.py
@@ -1,8 +1,8 @@
-import jraph
+import pyjraph
 
 if __name__ == "__main__":
     graph_file = "examples/graphs/graph.json"
-    graph = jraph.from_json(graph_file)
+    graph = pyjraph.from_json(graph_file)
 
     print("Graph class: ", graph.__class__)
     print("Graph functions: ", dir(graph))
